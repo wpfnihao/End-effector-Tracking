ef8134adae0221bea868f7254137af3647a7c9b6
wpfnihao <wpfnihao@gmail.com>
2014-06-19 16:13:10 +0800

    nightly

--- src/track_node.cpp
+++ src/track_node.cpp
@@ -19,6 +19,9 @@
 // file io
 #include <fstream>
 
+// for timing and boost the performance of the system
+#include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
+
 #define VIDEO 0
 #define ROS   1
 
@@ -110,119 +113,126 @@ int main(int argc, char **argv)
 	//the ros initial steps are done in the constructor, so only instantiate the CEndeffectorTracking class is sufficient
 	std::string name = "endeffector_tracking";
 
-omp_set_nested(1);
-#pragma omp parallel sections
+	omp_set_nested(1);
+#pragma omp parallel 
 	{
-#pragma omp section
+#pragma omp sections nowait
 		{
-			// for real-time frame processing
-			// using the ros
-			//
-			
-			//ros::init(argc, argv, name);
-
-			///* ROS params */
-			//ros::NodeHandle n;
-
-
-			////get param from the launch file or command line
-
-			////param([param_name], [member_variable], [default_value])
-
-			////FIXME:the param namespace is weird, how to figure out the namespace of an param?
-			//n.param<std::string>("endeffector_tracking/camera_topic", camera_topic, "camera/image_raw");
-			////the config_file containing the info of the cam
-			//n.param<std::string>("endeffector_tracking/config_file", config_file, "config.xml");
-			//n.param<std::string>("endeffector_tracking/model_name", model_name, "config.cao");
-			//n.param<std::string>("endeffector_tracking/init_file", init_file, "config.init");
-			//		
-			//image_transport::ImageTransport it(n);
-			//cv::VideoCapture cap(0); // open the default video file
-			cv::VideoCapture cap("/home/pengfei/Desktop/6.avi"); // open the default video file
-			cv::Mat tmp;
-			for (int i = 0; i < 174; i++)
-				cap >> tmp;
-
-
-
-			int MODE = VIDEO;
-			std::ofstream f1("pose_tracked.log");
-			switch (MODE)
+#pragma omp section
 			{
-				case VIDEO:
-					config_file = "config.xml";
-					model_name = "config.cao";
-					init_file = "config.init";
-					// capture frames
-							
-					if(cap.isOpened()) // check if we succeeded
-						for (;;)
-						{
-							// retrieve image from the camera
-							cv::Mat curImg;
-							cap >> curImg; // get a new frame from camera
-							if(curImg.empty())
+				// for real-time frame processing
+				// using the ros
+				//
+
+				//ros::init(argc, argv, name);
+
+				///* ROS params */
+				//ros::NodeHandle n;
+
+
+				////get param from the launch file or command line
+
+				////param([param_name], [member_variable], [default_value])
+
+				////FIXME:the param namespace is weird, how to figure out the namespace of an param?
+				//n.param<std::string>("endeffector_tracking/camera_topic", camera_topic, "camera/image_raw");
+				////the config_file containing the info of the cam
+				//n.param<std::string>("endeffector_tracking/config_file", config_file, "config.xml");
+				//n.param<std::string>("endeffector_tracking/model_name", model_name, "config.cao");
+				//n.param<std::string>("endeffector_tracking/init_file", init_file, "config.init");
+				//		
+				//image_transport::ImageTransport it(n);
+				//cv::VideoCapture cap(0); // open the default video file
+				cv::VideoCapture cap("/home/pengfei/Desktop/6.avi"); // open the default video file
+				cv::Mat tmp;
+				for (int i = 0; i < 174; i++)
+					cap >> tmp;
+
+
+
+				int MODE = VIDEO;
+				std::ofstream f1("pose_tracked.log");
+				switch (MODE)
+				{
+					case VIDEO:
+						config_file = "config.xml";
+						model_name = "config.cao";
+						init_file = "config.init";
+						// capture frames
+
+						if(cap.isOpened()) // check if we succeeded
+							for (;;)
 							{
-								std::cout<<"The tracking procedure finished successfully!"<<std::endl;
-								break;
-							}
-							//tracking based on different status
-							if (status == LOST || status == INITIAL)
-							{
-								// init the model from file
-								// only requires when initializing
-								if (status == INITIAL)
+								// retrieve image from the camera
+								cv::Mat curImg;
+								cap >> curImg; // get a new frame from camera
+								if(curImg.empty())
 								{
-									// get some basic info about the video
-									cv::Mat grayImg;
-									cv::cvtColor(curImg, grayImg, CV_BGR2GRAY);
-									rows = grayImg.rows;
-									cols = grayImg.cols;
-
-									//initializeTracker(srcImg);
-									srTracker.initialization(curImg, config_file, model_name, init_file);
-									srTracker.vpMbTracker::getPose(cMo);
+									std::cout<<"The tracking procedure finished successfully!"<<std::endl;
+									break;
 								}
-								//finish the initialization step and start to track
-								status = TRACKING;
-							}	
-							else if (status == TRACKING)
-							{
-								srTracker.retrieveImage(curImg);
-								srTracker.track();
-								srTracker.vpMbTracker::getPose(cMo);
+								//tracking based on different status
+								if (status == LOST || status == INITIAL)
+								{
+									// init the model from file
+									// only requires when initializing
+									if (status == INITIAL)
+									{
+										// get some basic info about the video
+										cv::Mat grayImg;
+										cv::cvtColor(curImg, grayImg, CV_BGR2GRAY);
+										rows = grayImg.rows;
+										cols = grayImg.cols;
+
+										//initializeTracker(srcImg);
+										srTracker.initialization(curImg, config_file, model_name, init_file);
+										srTracker.vpMbTracker::getPose(cMo);
+									}
+									//finish the initialization step and start to track
+									status = TRACKING;
+								}	
+								else if (status == TRACKING)
+								{
+									clock_t t; //timing
+									t = clock();
+									srTracker.retrieveImage(curImg);
+									srTracker.track();
+									srTracker.vpMbTracker::getPose(cMo);
+									t = clock() - t;
+									std::cout<<"single tracking iteration consumes "<<((float)t)/CLOCKS_PER_SEC<<" seconds."<<std::endl;
+								}
+
+								// save the pose into the file
+								poseVector.buildFrom(cMo);
+
+								f1<<poseVector[0]<<" "<<poseVector[1]<<" "<<poseVector[2]<<" "<<poseVector[3]<<" "<<poseVector[4]<<" "<<poseVector[5]<<" "<<std::endl;
 							}
+						break;
+						//case ROS:
+						//	// subCam is member variable subscribe to the msg published by the camera
+						//	// trackCallback do the tracking procedure
+						//	// the callback function is a member function of a class, so we should point out which class it belongs to explicitly
+						//	subCam = it.subscribe(camera_topic, 1, trackCallback);
 
-							// save the pose into the file
-							poseVector.buildFrom(cMo);
-
-							f1<<poseVector[0]<<" "<<poseVector[1]<<" "<<poseVector[2]<<" "<<poseVector[3]<<" "<<poseVector[4]<<" "<<poseVector[5]<<" "<<std::endl;
-						}
-					break;
-				//case ROS:
-				//	// subCam is member variable subscribe to the msg published by the camera
-				//	// trackCallback do the tracking procedure
-				//	// the callback function is a member function of a class, so we should point out which class it belongs to explicitly
-				//	subCam = it.subscribe(camera_topic, 1, trackCallback);
-
-				//	// publish message
-				//	imgPub = it.advertise("result", 1);
-				//	ros::spin();
-				//	break;
-				default:
-					break;
+						//	// publish message
+						//	imgPub = it.advertise("result", 1);
+						//	ros::spin();
+						//	break;
+					default:
+						break;
+				}
+				f1.close();
 			}
-			f1.close();
-		}
 
 #pragma omp section
-		{
-			// for super-resolution dataset processing
-			while (true)
-				srTracker.updateDataBase();
+			{
+				// for super-resolution dataset processing
+				while (true)
+					srTracker.updateDataBase();
+			}
 		}
 	}
-	return 0;
+		return 0;
 }
 
 /**
